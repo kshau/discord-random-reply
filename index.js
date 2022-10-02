ad0f70a04115ad59f2e67335fc2f1841899e52b8
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2022-10-01 20:03:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,61 +7,69 @@ const TOKEN = "NzY4MTgxMjc3ODE0Njg1NzA2.Glj2dn.9J7WSQslE6elSa6WSeD1avgO4qcy5w9Ui
 Fs.readFile("replies.txt", (err, data) => {
 
     const REPLIES_LIST = data.toString().split("\n")
+    
+    function connect() {
+    
+        var ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
+        var interval;
 
-    var ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
-    var interval;
+        ws.on("open", () => {
 
-    ws.on("open", () => {
+        ws.send(JSON.stringify({
+            "op": 2,
+            "d": {
+            "token": TOKEN,
+            "intents": 513,
+            "properties": {
+                "os": "linux",
+                "browser": "chrome"
+            }
+            }
+        }))
 
-    ws.send(JSON.stringify({
-        "op": 2,
-        "d": {
-        "token": TOKEN,
-        "intents": 513,
-        "properties": {
-            "os": "linux",
-            "browser": "chrome"
-        }
-        }
-    }))
-
-    })
+        })
 
 
-    ws.on("message", (data) => {
+        ws.on("message", (data) => {
 
-        var json = JSON.parse(data);
+            var json = JSON.parse(data);
 
-        var {op, t, d} = json;
+            var {op, t, d} = json;
 
-        if (op == 10) {
-            interval = d.heartbeat_interval;
-            setInterval(ms => {
-                ws.send(JSON.stringify({
-                op: 1,
-                d: null
-                }))
-            }, interval)
-        }
+            if (op == 10) {
+                interval = d.heartbeat_interval;
+                setInterval(ms => {
+                    ws.send(JSON.stringify({
+                    op: 1,
+                    d: null
+                    }))
+                }, interval)
+            }
 
-        switch (t) {
+            switch (t) {
 
-            case "MESSAGE_CREATE":
-                
-                if (d.author.id != "768181277814685706") {
-                    fetch(`https://discord.com/api/v9/channels/${d.channel_id}/messages`, {
-                        "headers": {
-                            "authorization": TOKEN,
-                            "content-type": "application/json"
-                        },
-                        "body": JSON.stringify({"content": REPLIES_LIST[Math.floor((Math.random() * REPLIES_LIST.length))]}),
-                        "method": "POST",
-                    }).then();
-                }
+                case "MESSAGE_CREATE":
 
-                break;
+                    if (d.author.id != "768181277814685706") {
+                        fetch(`https://discord.com/api/v9/channels/${d.channel_id}/messages`, {
+                            "headers": {
+                                "authorization": TOKEN,
+                                "content-type": "application/json"
+                            },
+                            "body": JSON.stringify({"content": REPLIES_LIST[Math.floor((Math.random() * REPLIES_LIST.length))]}),
+                            "method": "POST",
+                        }).then();
+                    }
 
-        }
+                    break;
+
+            }
+
+        })
         
-    })
+        connect();
+        
+    }
 })
+
+connect();
